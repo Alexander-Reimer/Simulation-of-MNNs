ea91f98ac3071d7c5a2af0534515df00d06d6b18
Alexander Reimer <alexander.reimer2357@gmail.com>
2023-11-05 12:23:51 +0100

    refactored

--- src/MNN.jl
+++ src/MNN.jl
@@ -1,13 +1,16 @@
 module MNN
 
-# using GLMakie
+using GLMakie
 using Graphs
 using LinearAlgebra
 using MetaGraphsNext
+using Observables
 
-struct Neuron
+mutable struct Neuron
     # https://jkrumbiegel.com/pages/2020-10-31-tuples-and-vectors/
-    pos::Tuple{Float64, Float64}
+    # TODO: cause of error?
+    # pos::Tuple{Float64, Float64}
+    pos::Array{Float64,1}
     velocity::Vector{Float64}
     movable::Bool
 end
@@ -18,28 +21,45 @@ struct Spring
 end
 spring_init() = rand() - 0.5
 
+mutable struct Visualizer
+    neuron_xs::Observables.Observable{Vector{Float64}}
+    neuron_ys::Observables.Observable{Vector{Float64}}
+    edge_pairs::Vector{Tuple}
+    observers::Dict{Any,Observables.Observable}
+    fig
+    ax
+end
+
 mutable struct Network
     graph::MetaGraphsNext.MetaGraph
-    rows::Int
+    rows::Int # how many rows fixed columns have
+    row_counts::Array{Int,1}
+    col_fixed::Array{Bool,1}
     columns::Int
     neuron_count::Int
+    xdist::Float64
+    ydist::Float64
+end
+
+function Network(graph::MetaGraphsNext.MetaGraph, rows::Int, columns::Int, xdist::Float64=1.0)
+    row_counts = Array{Int}(undef, columns)
+    col_fixed = Array{Bool}(undef, columns)
+    neuron_count = 0
+    Network(graph, rows, row_counts, col_fixed, columns, neuron_count, xdist, calc_ydist(xdist))
 end
 
 function print_graph(network::Network)
-    for n in network.graph.vertex_properties |> values
-        println("Pos: $(n[2].pos), movable: $(n[2].movable)")
+    for col in 1:network.columns
+        println("Column: $col")
+        for row in 1:network.row_counts[col]
+            i = get_neuron_index(network, col, row)
+            n = get_neuron(network, i)
+            println("\t Pos: $(n.pos), movable: $(n.movable)")
+        end
     end
-end
-
-function add_column!(network::Network, rows::Int, column::Int, movable::Bool, yoffset=sqrt(0.75)*2, xoffset=1)
-    function add_neuron!(movable::Bool, xpos::Number, ypos::Number, row::Number)
-        network.graph[network.neuron_count] = Neuron((xpos, ypos), [0, 0], movable)
-        network.neuron_count += 1
-    end
-    for row in 1:rows
-        movable_neuron = movable || !(row == 1 || row == rows)
-        add_neuron!(movable_neuron, (column-1) * xoffset, (row-1) * yoffset, row)
-    end
+    # for n in network.graph.vertex_properties |> values
+    #     println("Pos: $(n[2].pos), movable: $(n[2].movable)")
+    # end
 end
 
 
@@ -53,17 +73,136 @@ function new_graph()
     )
 end
 
-function initialize_graph!(network::Network)
-    yoffset = sqrt(0.75)
-    for column in 1:network.columns
-        add_column!(network, network.rows, column, false)
+function get_neuron_index(network::Network, column::Int, row::Int)
+    return sum(network.row_counts[1:column-1]) + row
+end
+
+# label: whether index is "true" (-> internal) index or "label" given by user at
+# definition
+function get_neuron(network::Network, index::Int; label::Bool=true)
+    true_index = label ? network.graph.vertex_labels[index] : index
+    # Tuple of [1] index and [2] neuron
+    x = network.graph.vertex_properties[true_index]
+    x[1] != index && error("Error correctly converting index")
+    return x[2]
+end
+
+function get_neuron(network::Network, column::Int, row::Int)
+    return get_neuron(network, get_neuron_index(network, column, row))
+end
+
+function set_neuron_positions!(network::Network, column::Int)
+    xdist = network.xdist
+    ydist = network.ydist
+    fixed = network.col_fixed[column]
+    rows = network.row_counts[column]
+    for row in 1:rows
+        xpos = (column - 1) * xdist
+        ypos = (row - 1) * ydist
+        if !fixed
+            ypos += ydist / 2
+        end
+
+        i = get_neuron_index(network, column, row)
+        neuron = get_neuron(network, i)
+        neuron.pos[1] = xpos
+        neuron.pos[2] = ypos
     end
+end
+
+function set_neuron_velocities!(network::Network, column::Int)
+    for row in network.row_counts[column]
+        get_neuron(network, column, row).velocity[1] = 0
+        get_neuron(network, column, row).velocity[2] = 0
+    end
+end
+
+function set_neuron_velocities!(network::Network)
+    for n in 1:network.neuron_count
+        get_neuron(network, n).velocity[1] = 0
+        get_neuron(network, n).velocity[2] = 0
+    end
+end
+
+function set_neuron_positions!(network::Network)
+    for col in 1:network.columns
+        set_neuron_positions!(network, col)
+    end
+end
+
+function initialize_neurons!(network::Network)
+    function add_column!(rows::Int, column::Int, fixed::Bool)
+
+        function add_neuron!(movable::Bool, xpos::Number, ypos::Number)
+            network.neuron_count += 1
+            network.graph[network.neuron_count] = Neuron([xpos, ypos], [0, 0], movable)
+        end
+
+        network.row_counts[column] = rows
+        network.col_fixed[column] = fixed
+
+        for row in 1:rows
+            movable_neuron = !(fixed && (row == 1 || row == rows))
+            add_neuron!(movable_neuron, 0, 0)
+        end
+    end
+
+    fixed = false
+    for column in 1:network.columns
+        rows = fixed ? network.rows : network.rows - 1
+        add_column!(rows, column, fixed)
+        fixed = !fixed
+        set_neuron_positions!(network, column)
+    end
+end
+
+function initialize_springs!(network::Network)
+    function add_spring!(n1::Int, n2::Int)
+        pos1 = get_neuron(network, n1).pos
+        pos2 = get_neuron(network, n2).pos
+        dx = pos1[1] - pos2[1]
+        dy = pos1[2] - pos2[2]
+        len = sqrt(dx^2 + dy^2)
+        return add_edge!(network.graph, n1, n2, Spring(spring_init(), len))
+    end
+    function good_index(col, row)
+        return 1 <= col <= network.columns && 1 <= row <= network.row_counts[col]
+    end
+    function add_spring!(col1, row1, col2, row2)
+        if good_index(col1, row1) && good_index(col2, row2)
+            add_spring!(
+                get_neuron_index(network, col1, row1),
+                get_neuron_index(network, col2, row2)
+            )
+        end
+    end
+
+    for col in 1:network.columns
+        fixed = network.col_fixed[col]
+        rows = network.row_counts[col]
+        for row in 1:rows
+            add_spring!(col, row, col + 1, row)
+            if !(fixed && (row == 1 || row == rows))
+                add_spring!(col, row, col + 2, row)
+            end
+            if fixed
+                add_spring!(col, row, col + 1, row - 1)
+            else
+                add_spring!(col, row, col + 1, row + 1)
+            end
+        end
+    end
+end
+
+function initialize_graph!(network::Network)
+    initialize_neurons!(network)
+    initialize_springs!(network)
     print_graph(network)
-    
-    res = true
-    # TODO
-    rows = network.rows
-    cols = network.columns
+
+    # res = true
+    # # TODO
+    # rows = network.rows
+    # cols = network.columns
     # for col in 2:cols
     #     for row1 in 1:rows
     #         for row2 in 1:rows
@@ -87,16 +226,178 @@ function initialize_graph!(network::Network)
     #         res = res && add_edge!(network.graph, i, i-rows-1, Spring(r_spring_constant(),1))
     #     end
     # end
-    return res
+    # return res
 end
+
+"""
+    calc_ydist(xdist)
+
+Calculate necessary distance on y axis to create an equilateral triangle with
+given distance between columns on x axis.
+"""
+calc_ydist(xdist) = 2 * sqrt(3) * xdist
 
 function Network(columns, rows)
     graph = new_graph()
-    network = Network(graph, rows, columns, 1)
-    res = initialize_graph!(network)
-    @info "Successful initialization?" res
+    network = Network(graph, rows, columns)
+    initialize_graph!(network)
+    # @info "Successful initialization?" res
     print_graph(network)
     return network
+end
+
+function draw(network::Network)
+    fig = Figure()
+    ax = Axis(fig[1, 1])
+
+    # edges first so they dont cover neurons
+    # edges
+    global edge_draw_obs = []
+    i = 1
+    for (n1, n2) in keys(network.graph.edge_data)
+        xs = Observable(
+            [get_neuron(network, n1).pos[1], get_neuron(network, n2).pos[1]],
+        # ignore_equal_values=true
+        )
+        push!(edge_draw_obs, xs)
+        ys = Observable(
+            [get_neuron(network, n1).pos[2], get_neuron(network, n2).pos[2]],
+        # ignore_equal_values=true
+        )
+        push!(edge_draw_obs, ys)
+        lines!(edge_draw_obs[i], edge_draw_obs[i+1], color=:red, stroke=10)
+        i += 2
+    end
+
+    # neurons
+    xs = [get_neuron(network, i).pos[1] for i in 1:sum(network.row_counts)]
+    ys = [get_neuron(network, i).pos[2] for i in 1:sum(network.row_counts)]
+    scatter!(xs, ys, marker=:circle, markersize=25, color=:blue)
+end
+
+function get_spring(network, n1, n2; label::Bool=true)
+    tn1 = label ? network.graph.vertex_labels[n1] : n1
+    tn2 = label ? network.graph.vertex_labels[n2] : n2
+    if !haskey(network.graph.edge_data, (tn1, tn2))
+        return network.graph.edge_data[tn2, tn1]
+    end
+    return network.graph.edge_data[tn1, tn2]
+end
+
+function displacement2force(diff, spring_constant)
+    if abs(diff) > 0.1
+        return diff * 100
+    else
+        return diff * spring_constant
+    end
+end
+
+function calculate_force(network::Network, n::Int)
+    n = network.graph.vertex_labels[n]
+    f = [0, 0]
+    # neighbouring neurons
+    nneurons = neighbors(network.graph, n)
+    for nn in nneurons
+        neuron = get_neuron(network, n, label=false)
+        nneuron = get_neuron(network, nn, label=false)
+        spring = get_spring(network, n, nn, label=false)
+        diff = neuron.pos - nneuron.pos
+        f += displacement2force(spring.length - norm(diff),
+            spring.spring_constant) * diff / norm(diff)
+        if isnan.(f) != [0, 0]
+            throw("calculated force as NaN at neighboring neuron $nn with pos $(neuron.pos)")
+        end
+    end
+    return f
+end
+
+function update_position!(network::Network, n::Int, delta::Number)
+    neuron = get_neuron(network, n)
+    if neuron.movable == false
+        return
+    end
+    f = calculate_force(network, n) * delta
+    # @info "f: $(round.(f, digits=2))"
+    neuron.velocity += f
+    neuron.velocity *= 0.95
+    # @info "v: $(round.(neuron.velocity, digits=3))"
+    neuron.pos += neuron.velocity * delta
+end
+
+function simulate!(network::Network, delta::Number, epochs::Int;
+    vis::Union{Visualizer,Nothing}=nothing)
+    for i = 1:epochs/10
+        start_time = time()
+        for j = 1:10
+            for row in 1:network.row_counts[1]
+                get_neuron(network, 0, row).velocity[1] += 0.5 / (1 / delta)
+            end
+            for n in 1:network.neuron_count
+                update_position!(network, n, delta)
+            end
+            if vis !== nothing
+                update_positions!(vis, network)
+                sleep(0.005)
+            end
+        end
+        fps = 10 / (time() - start_time)
+        @info "FPS: $fps"
+        # TODO: adjust delta automatically dependant on fps?
+    end
+end
+
+function update_positions!(vis::Visualizer, network::Network)
+    for n in 1:network.neuron_count
+        neuron = get_neuron(network, n)
+        vis.neuron_xs.val[n] = neuron.pos[1]
+        vis.neuron_ys.val[n] = neuron.pos[2]
+    end
+    notify(vis.neuron_xs)
+    notify(vis.neuron_ys)
+    for (n1, n2, ax) in keys(vis.observers)
+        if ax == :xs
+            vis.observers[n1, n2, :xs].val[1] = vis.neuron_xs.val[n1]
+            vis.observers[n1, n2, :xs].val[2] = vis.neuron_xs.val[n2]
+        else
+            vis.observers[n1, n2, :ys].val[1] = vis.neuron_ys.val[n1]
+            vis.observers[n1, n2, :ys].val[2] = vis.neuron_ys.val[n2]
+        end
+        notify(vis.observers[n1, n2, ax])
+    end
+end
+
+function Visualizer(network::Network; max_fps::Number = 10)
+    @info "test"
+    neuron_xs = Observable(Vector{Float64}(undef, network.neuron_count), ignore_equal_values=true)
+    neuron_ys = Observable(Vector{Float64}(undef, network.neuron_count), ignore_equal_values=true)
+    throttle(1/max_fps, neuron_xs)
+    throttle(1/max_fps, neuron_ys)
+    edge_pairs = collect(keys(network.graph.edge_data))
+
+    fig = Figure()
+    ax = Axis(fig[1, 1])
+    observers = Dict()
+    vis = Visualizer(neuron_xs, neuron_ys, edge_pairs, observers, fig, ax)
+
+    update_positions!(vis, network)
+    scatter!(neuron_xs, neuron_ys, marker=:circle, markersize=25, color=:blue)
+
+    for (n1, n2) in edge_pairs
+        vis.observers[n1, n2, :xs] = Observable([neuron_xs.val[n1], neuron_xs.val[n2]])
+        throttle(1/max_fps, vis.observers[n1, n2, :xs])
+        vis.observers[n1, n2, :ys] = Observable([neuron_ys.val[n1], neuron_ys.val[n2]])
+        throttle(1/max_fps, vis.observers[n1, n2, :ys])
+        l = lines!(vis.observers[n1, n2, :xs], vis.observers[n1, n2, :ys], color=:red, stroke=10)
+        # break
+    end
+
+    display(fig)
+    return vis
+end
+
+function reset!(network::Network)
+    set_neuron_positions!(network)
+    set_neuron_velocities!(network)
 end
 
 end
