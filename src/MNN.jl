04d965867b7f195d9be79cc3c22fb81dd1cb1d10
Matteo Friedrich <matteo.r.friedrich@gmail.com>
2023-12-10 18:20:51 +0100

    Added Backpropagation, not working properly

--- src/MNN.jl
+++ src/MNN.jl
@@ -1,4 +1,4 @@
-module MNN
+#module MNN
 
 export Network, simulate!, Trainer, train!, Visualizer, netpush!, netpull!, draw!, reset!
 
@@ -214,7 +214,7 @@ end
 function initialize_graph!(network::Network)
     initialize_neurons!(network)
     initialize_springs!(network)
-    print_graph(network)
+    #print_graph(network)
 end
 
 """
@@ -596,6 +596,68 @@ function calc_losses_parallel!(network, candidates, losses,
     end
 end
 
+function calculate_vec_align(v1, v2)
+    return clamp(v1⋅v2/(norm(v1)*norm(v2)), -1, 1)
+end
+
+function train_epoch!(network, trainer)
+    behaviours = trainer.behaviours
+    reset!(network)
+    simulate!(network, (0.0,100.0), modifier = behaviours[1].modifier)
+    deltas = calculate_deltas(network, behaviours[1])
+    for spring in edges(network.graph)
+        update_spring!(n, spring, deltas)
+    end
+    l = loss(network, behaviours[1])
+    println("Start loss: ", l)
+    return l
+end
+
+function update_spring!(n, spring, deltas)
+    if !n.graph[spring.dst].movable
+        return
+    end
+
+    delta = deltas[:,spring.dst]
+    dif = n.positions[:, spring.src] - n.positions[:, spring.dst]
+    dist = norm(dif)
+    f = springforce((dist - sqrt(n.xdist^2 + n.ydist^2)), n.graph[spring.src, spring.dst].spring_constant)  * dif / dist
+    align = calculate_vec_align(delta, f)
+    #n.graph[spring.src, spring.dst].spring_constant += (dist - sqrt(n.xdist^2 + n.ydist^2)) * align * 0.00005
+    n.graph[spring.src, spring.dst].spring_constant += align * 0.00005
+    #println((dist - sqrt(n.xdist^2 + n.ydist^2)) * align * 0.05)
+end
+
+function calculate_deltas(network, behaiviour)
+    deltas = zeros(Float64, size(network.positions)[1], size(network.positions)[2])
+    for goal in behaiviour.goals
+        for (neuron_i, goal_pos) in behaiviour.goals
+            neuron = get_neuron(network, neuron_i)
+            if behaiviour.relative
+                start_pos = network.start_positions[neuron_i]
+                goal_pos += start_pos
+            end
+            deltas[:, neuron_i] = (goal_pos - network.positions[:, neuron_i])
+        end
+    end
+
+    for col in (network.columns)-1:-1:1  
+        for row in 1:network.row_counts[col]
+            i = get_neuron_index(network, col, row)
+            if network.graph[i].movable
+                neuros = neighbors(network.graph, i)
+                for neuro in neuros
+                    if i<neuro
+                        deltas[:, i] += deltas[:, neuro]/3
+                    end
+                end
+            end
+        end
+    end
+
+    return deltas
+end
+
 function train!(network::Network, epochs::Int, behaviours::Vector{Behaviour};
     vis=nothing, mutations=20, parallel=true, mutation_strength=0.3,
     simepochs=250)
@@ -609,8 +671,9 @@ function train!(network::Network, epochs::Int, behaviours::Vector{Behaviour};
     candidates = Array{Dict{Tuple{Int64,Int64},Spring}}(undef, mutations)
     [candidates[i] = Dict() for i in eachindex(candidates)]
     candidate_losses = Vector{Float64}(undef, mutations)
-    best_candidate = copy(get_spring_constants(network))
+    best_candidate = deepcopy(get_spring_constants(network))
     best_loss = calc_loss(network, best_candidate, behaviours, epochs=simepochs)
+    println(best_loss)
     for i = 1:epochs
         create_mutations!(best_candidate, candidates, strength=mutation_strength)
         loss_function!(network, candidates, candidate_losses, behaviours;
@@ -618,7 +681,7 @@ function train!(network::Network, epochs::Int, behaviours::Vector{Behaviour};
         best_i = argmin(candidate_losses)
         if candidate_losses[best_i] < best_loss
             best_loss = candidate_losses[best_i]
-            best_candidate = deepcopy(candidates[i])
+            best_candidate = deepcopy(candidates[best_i])
             @info "\tUpdated!"
         end
         @info "Epoch: $i, best loss: $(best_loss)"
@@ -629,7 +692,7 @@ function train!(network::Network, epochs::Int, behaviours::Vector{Behaviour};
 end
 
 function train!(network::Network, epochs::Int, trainer::Trainer;
-    vis=nothing, mutations=20, parallel=true, mutation_strength=0.3,
+    vis=nothing, mutations=20, parallel=false, mutation_strength=0.3,
     simepochs=250)
     train!(network, epochs, trainer.behaviours, vis=vis, mutations=mutations,
         parallel=parallel, mutation_strength=mutation_strength, simepochs=simepochs)
@@ -660,7 +723,7 @@ function Trainer(network::Network)
     behaviour_push.goals[mid_neuron_i] = [1.0, 0.0]
     behaviour_push.modifier = netpull!
 
-    return Trainer([behaviour_pull, behaviour_pull])
+    return Trainer([behaviour_pull, behaviour_push])
 end
 
-end
+#end
