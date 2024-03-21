34180f56e298536283643f311980c74bafee513f
Alexander Reimer <alexander.reimer2357@gmail.com>
2024-03-21 12:03:00 +0100

    improved visualization (color for spring constant values; draw thick lines top + bottom)

--- src/Visualize.jl
+++ src/Visualize.jl
@@ -57,15 +57,50 @@ function Visualizer(network::Network; max_fps::Number=10, behaviour=nothing)
 
     update_positions!(vis, network)
 
+    spring_costants = get_spring_constants_vec(network)
+    max_color_dist = maximum(abs.((minimum(spring_costants), maximum(spring_costants))))
+    colorrange = (-max_color_dist, max_color_dist)
     for (n1, n2) in edge_pairs
         vis.observers[n1, n2, :xs] = Observable([neuron_xs.val[n1], neuron_xs.val[n2]])
         throttle(1 / max_fps, vis.observers[n1, n2, :xs])
         vis.observers[n1, n2, :ys] = Observable([neuron_ys.val[n1], neuron_ys.val[n2]])
         throttle(1 / max_fps, vis.observers[n1, n2, :ys])
-        l = lines!(vis.observers[n1, n2, :xs], vis.observers[n1, n2, :ys]; color=:grey)
+        spring = network.graph.edge_data[n1, n2]
+        l = lines!(
+            vis.observers[n1, n2, :xs],
+            vis.observers[n1, n2, :ys];
+            color=spring.spring_constant,
+            # colormap=:diverging_bkr_55_10_c35_n256,
+            colormap=[:blue, :gray, :red],
+            colorrange=colorrange,
+        )
     end
 
     scatter!(neuron_xs, neuron_ys; marker=:circle, color=:black)
+
+    first_col = network.columns + 1
+    last_col = 0
+    for col in 1:(network.columns)
+        if network.col_fixed[col]
+            if col < first_col
+                first_col = col
+            end
+            if col > last_col
+                last_col = col
+            end
+        end
+    end
+
+    for row in [1, network.rows]
+        n1 = get_neuron_index(network, first_col, row)
+        n2 = get_neuron_index(network, last_col, row)
+        lines!(
+            [neuron_xs.val[n1], neuron_xs.val[n2]],
+            [neuron_ys.val[n1], neuron_ys.val[n2]];
+            color=:black,
+            linewidth=3,
+        )
+    end
 
     if behaviour !== nothing
         for (n, coords) in behaviour.goals
