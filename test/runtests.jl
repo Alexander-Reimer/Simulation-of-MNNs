1e51b87a3b56c0538a71887cdf1c7285d660e975
Alexander Reimer <alexander.reimer2357@gmail.com>
2024-04-10 21:18:21 +0200

    fixed test fails

--- test/runtests.jl
+++ test/runtests.jl
@@ -5,11 +5,11 @@ using Test
     @testset "Network" begin
         net = Network(5, 4)
         @test net.columns == 5
-        @test net.rows = 4
+        @test net.rows == 4
         @test net.neuron_count == 17
-        @test MNN.get_neuron(1, 1) == net.graph.vertex_properties[1]
-        @test MNN.get_neuron(5, 3) == net.graph.vertex_properties[net.neuron_count]
-        @test MNN.get_neuron(3, 2) == net.graph.vertex_properties[9]
+        @test (1, MNN.get_neuron(net, 1, 1)) == net.graph.vertex_properties[1]
+        @test (17, MNN.get_neuron(net, 5, 3)) == net.graph.vertex_properties[net.neuron_count]
+        @test (9, MNN.get_neuron(net, 3, 2)) == net.graph.vertex_properties[9]
     end
     @testset "Deformation" begin
         net = Network(5, 4)
