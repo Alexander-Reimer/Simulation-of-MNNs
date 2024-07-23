105eef2472bc2376019fcfc9491fc0f4ed5d59bb
Alexander-Reimer <alexander.reimer2357@gmail.com>
2024-07-23 18:42:59 +0200

    changed julia version to 1.10.3; fixed error with nix shell not finding libquadmath.so.0

--- default.nix
+++ default.nix
@@ -2,8 +2,8 @@
 
 pkgs.mkShell {
   buildInputs = [ ];
-
+  nativeBuildInputs = with pkgs.buildPackages; [ julia ];
   shellHook = ''
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/run/opengl-driver/lib:/run/opengl-driver-32/lib"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/run/opengl-driver/lib:/run/opengl-driver-32/lib:${pkgs.buildPackages.julia}/lib/julia"
   '';
 }
