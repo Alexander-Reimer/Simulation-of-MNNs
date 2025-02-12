70c295e7f7d0fe35b9dcd3e794091bd3d6906fcb
Alexander-Reimer <alexander.reimer2357@gmail.com>
2025-02-12 13:51:36 +0100

    fixed SSL CURL error

--- default.nix
+++ default.nix
@@ -1,9 +1,17 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell {
   buildInputs = [ ];
-  nativeBuildInputs = with pkgs.buildPackages; [ julia ];
+  nativeBuildInputs = with pkgs; [ julia ];
+  # load correct (current) project environment automatically
+  JULIA_PROJECT = "@.";
+  # adding julia lib path to "global" LD_LIBRARY_PATH causes issues as those
+  # libs are chosen over correct ones, so it is instead only added when running
+  # the julia command
   shellHook = ''
-    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/run/opengl-driver/lib:/run/opengl-driver-32/lib:${pkgs.buildPackages.julia}/lib/julia"
+    export LD_LIBRARY_PATH="$LD_LIBRARY_PATH:/run/opengl-driver/lib:/run/opengl-driver-32/lib"
+    alias julia='LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${pkgs.julia}/lib/julia" julia'
   '';
 }
